d25fa9de03bf965c7ab978084b565ba990e05223
xolani <mcxolani@gmail.com>
2021-06-22 14:31:59 +0200

    add api tests

--- weather/tests.py
+++ weather/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class ApiTestCase(APITestCase):
+    def test_errors_city_missing(self):
+        """
+        Make sure returns the right error data
+        """
+        url = reverse('locations-detail', args=(' ',))
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data = response.json()
+        self.assertEqual(data['message'], 'No matching location found.')
+
+    def test_success(self):
+        """
+        Make sure returns the right error data
+        """
+        url = reverse('locations-detail', args=('London',))
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+        self.assertTrue(data['maximum'])
+        self.assertTrue(data['average'])
+        self.assertTrue(data['minimum'])
+        self.assertTrue(data['median'])
